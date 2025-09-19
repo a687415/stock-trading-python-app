a78785aacccd5f295cbdf43c9dfea799fff9c8d5
a687415 <130870366+a687415@users.noreply.github.com>
2025-09-19 15:45:31 -0400

    Create script.py

---
+++ script.py
@@ -0,0 +1,46 @@
+import requests
+import os
+import csv
+from datetime import datetime
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def run_stock_job():
+    count = 1
+    API_KEY = os.getenv("POLYGON_API_KEY")
+    LIMIT = os.getenv("API_PAGINATION_LIMIT")
+    tickers = []
+    url = f"https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&sort=ticker&order=asc&limit={LIMIT}&apiKey={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+
+    for ticker in data['results']:
+        tickers.append(ticker)
+
+    while count < 5 and data.get('next_url'):
+        print(f"Fetching next page:", data['next_url'])
+        next_url = data['next_url'] + f"&apiKey={API_KEY}"
+        response = requests.get(next_url)
+        data = response.json()
+        for ticker in data['results']:
+            tickers.append(ticker)
+        count += 1
+
+    print(len(tickers))
+
+    # Write to CSV with schema defined for ticker_symbol
+    csv_columns = [
+        "ticker", "name", "market", "locale", "primary_exchange", "type", "active",
+        "currency_name", "cik", "composite_figi", "share_class_figi", "last_updated_utc"
+    ]
+    with open("tickers.csv", "w", newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+        writer.writeheader()
+        for ticker in tickers:
+            row = {col: ticker.get(col, "") for col in csv_columns}
+            writer.writerow(row)
+
+if __name__ == "__main__":
+    run_stock_job()
+
